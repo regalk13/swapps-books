fb87138dc3f211519562e70e2175627d8fc74c13
Regalk <jsalazarcaviedes30@gmail.com>
2024-02-06 20:26:43 -0500

    Hash Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 // import { useState } from 'react'
 import Home from "./Home.tsx";
 import BookDetails from "./BookDetails.tsx";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, HashRouter } from "react-router-dom";
 function App() {
   return (
     <>
-      <Router basename={"https://regalk13.github.io/swapps-books/"}>
+      <HashRouter>
         <div>
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/bookdetails" element={<BookDetails />} />
           </Routes>
         </div>
-      </Router>
+      </HashRouter>
     </>
   );
 }
